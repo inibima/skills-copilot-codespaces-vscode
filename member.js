b8f937960dba52071d19b6ffcc34bcd7777f4679
nugrahabima <linemasakini@gmail.com>
2025-01-29 04:30:38 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember()  {
+    var member = {
+        "name": "Member",
+        "skills": ["JavaScript", "HTML", "CSS"],
+        "experience": "5 years",
+        "team": "Development"
+    };
+    return member;
+}
